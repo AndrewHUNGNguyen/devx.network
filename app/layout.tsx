1db1a526dd89e7306b13e757f7fcfa0a09cda1c9
Sam Holmes <sam.lb.holmes@gmail.com>
2025-05-21 07:02:40 -0700

    Convert Header to use styled-components
    I'm incompetent with tailwindCSS, and it's meaningless to me. I couldn't
    event relocate the header to the left because tailwind is such garbage.
    I'm sorry, I'm not worthy of tailwind, so I'm using what I know.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react"
 import type { Metadata } from "next"
 import "./globals.css"
-import Header from "./components/Header"
+import { Header } from "./components/Header"
 import Footer from "./components/Footer"
 import { siteConfig } from "./siteConfig"
 import GiveATalkCTA from "./components/GiveATalkCTA"
