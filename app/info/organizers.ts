fed3811102127df6fa493fec3172454370948b4d
Sam Holmes <sam.lb.holmes@gmail.com>
2025-09-11 22:53:04 -0700

    Update profile photos

--- app/info/organizers.ts
+++ app/info/organizers.ts
@@ -3,22 +3,22 @@
 export const organizers = [
 	{
 		name: "AJ Caldwell",
-		imageSrc: "/images/aj-about.webp",
+		imageSrc: "/images/profile-photos/aj-2026.png",
 		linkedIn: "https://www.linkedin.com/in/aj-caldwell/"
 	},
 	{
 		name: "Ezekiel Lopez",
-		imageSrc: "/images/ez-about.webp",
+		imageSrc: "/images/profile-photos/ez-about.png",
 		linkedIn: "https://www.linkedin.com/in/ezekiel-lopez-633b57212/"
 	},
 	{
 		name: "Jonathan Lewis",
-		imageSrc: "/images/jon-about.webp",
+		imageSrc: "/images/profile-photos/jon-about.png",
 		linkedIn: "https://www.linkedin.com/in/jonathan-david-lewis/"
 	},
 	{
 		name: "Sam Holmes",
-		imageSrc: "/images/sam-about.webp",
+		imageSrc: "/images/profile-photos/sam-2026.png",
 		linkedIn: "https://www.linkedin.com/in/sammyholmes/"
 	}
 	//, {
